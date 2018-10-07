e34077e6d07e6ae1797d2260bede2c9521a9770e
Archit Dwivedi <architdwivedi.off@gmail.com>
2018-10-07 23:41:20 +0530

    Solution of problem 4 :tada:

--- pro-euler/p4.py
+++ pro-euler/p4.py
@@ -1,16 +1,21 @@
 
+
 def reve(t):
-    if len(t) is not 0:                           return reve(t[1:]) + t[0]               else:
+    if len(t) is not 0:                         
+       return reve(t[1:]) + t[0]              
+    else:
        return t
 
 def fac(i):
     f = 0
     for u in range(1000, 100, -1):
-        if (i%u == 0)*(i/u < 1000)*(i/u > 99):                                                    f = 1
+        if (i%u == 0)*(i/u < 1000)*(i/u > 99):                                 
+            f = 1
             break
     return f
 
-                                           for i in range(1000000, 100000, -1):
+
+for i in range(1000000, 100000, -1):
     s = str(i)
     if s == reve(s):
         if fac(i) is 1:
